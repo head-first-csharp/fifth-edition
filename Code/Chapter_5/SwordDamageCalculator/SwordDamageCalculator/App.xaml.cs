8fd9703d00f6ade08f0cc4c908aa5cd77c3d0cf3
andrewstellman <astellman@stellman-greene.com>
2023-06-25 18:23:25 -0400

    Added chapter 5 MAUI code

---
+++ Code/Chapter_5/SwordDamageCalculator/SwordDamageCalculator/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SwordDamageCalculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Code/Chapter_5/SwordDamageCalculator/SwordDamageCalculator/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SwordDamageCalculator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
