a428463e867ef78d8730c21379de4ea68f7cabf7
andrewstellman <astellman@stellman-greene.com>
2024-04-13 11:33:16 -0400

    Added Chapter 3 code

---
+++ Code/Chapter_3/Clown/Clown/Clown.cs
@@ -0,0 +1,11 @@
+﻿class Clown
+{
+    public string Name;
+    public int Height;
+
+    public void WhoAreYou()
+    {
+        Console.WriteLine("My name is " + Name +
+            " and I'm " + Height + " inches tall.");
+    }
+}
---
+++ Code/Chapter_3/Clown/Clown/Program.cs
@@ -0,0 +1,17 @@
+﻿Clown oneClown = new Clown();
+oneClown.Name = "Boffo";
+oneClown.Height = 14;
+oneClown.WhoAreYou();      // My name is _______ and I'm ____ inches tall.
+
+Clown anotherClown = new Clown();
+anotherClown.Name = "Biff";
+anotherClown.Height = 16;
+anotherClown.WhoAreYou();  // My name is _______ and I'm ____ inches tall.
+
+Clown clown3 = new Clown();
+clown3.Name = anotherClown.Name;
+clown3.Height = oneClown.Height - 3;
+clown3.WhoAreYou();        // My name is _______ and I'm ____ inches tall.
+
+anotherClown.Height *= 2;
+anotherClown.WhoAreYou();  // My name is _______ and I'm ____ inches tall.
