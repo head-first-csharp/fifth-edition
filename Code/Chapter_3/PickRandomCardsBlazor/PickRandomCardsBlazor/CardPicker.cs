c925e943a2102dc69a83ac577e3d3e72f1c0e400
andrewstellman <astellman@stellman-greene.com>
2024-07-21 19:38:39 -0400

    Added Blazor projects

---
+++ Code/Chapter_3/PickRandomCardsBlazor/PickRandomCardsBlazor/CardPicker.cs
@@ -0,0 +1,34 @@
+﻿
+namespace PickRandomCards;
+
+internal class CardPicker
+{
+    public static string[] PickSomeCards(int numberOfCards)
+    {
+        string[] pickedCards = new string[numberOfCards];
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            pickedCards[i] = RandomValue() + " of " + RandomSuit();
+        }
+        return pickedCards;
+    }
+
+    private static string RandomSuit()
+    {
+        int value = Random.Shared.Next(1, 5);  // pick a random number from 1 to 4
+        if (value == 1) return "Spades";       // if it's 1 return the string Spades
+        if (value == 2) return "Hearts";       // if it's 2 return the string Hearts
+        if (value == 3) return "Clubs";        // if it's 3 return the string Clubs
+        return "Diamonds";                     // otherwise return the string
+    }
+
+    private static string RandomValue()
+    {
+        int value = Random.Shared.Next(1, 14);
+        if (value == 1) return "Ace";
+        if (value == 11) return "Jack";
+        if (value == 12) return "Queen";
+        if (value == 13) return "King";
+        return value.ToString();
+    }
+}
